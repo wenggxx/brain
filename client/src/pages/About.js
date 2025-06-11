9b85f8d43b1a8c9133d62db5aa6c738a8d730245
wenggxx <xiaoxi.weng@gmail.com>
2025-06-11 11:59:10 -0400

    Fixed message display

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
+
 export default function About() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState([]);
 
-      useEffect(() => {
-        fetch('/api/hello')
-          .then(response => response.json())
-          .then(data => setMessage(data));
-      }, []);
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data));
+  }, []);
 
-      return (
-        <div>
-          <h1>{message.map(message => <div>{message.title}</div>)}</h1>
-        </div>
-      );
+  return (
+    <div>
+      <h1>Messages</h1>
+      <ul>
+        {message.map((row, index) => (
+          <li key={index}>
+            {Object.entries(row).map(([key, value]) => (
+              <div key={key}>
+                <strong>{key}:</strong> {value.toString()}
+              </div>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
