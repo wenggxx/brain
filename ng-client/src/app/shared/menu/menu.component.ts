0fb6a9bb06ebe7bdd09d585d0574feeee46d10d3
wenggxx <xiaoxi.weng@gmail.com>
2025-07-03 10:12:16 -0400

    Close hamburger

--- ng-client/src/app/shared/menu/menu.component.ts
+++ ng-client/src/app/shared/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ElementRef } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,17 +7,17 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   template: `
     <nav class="top-menu">
-      <div class="menu-left-wrapper" #menuWrapper>
+      <div class="menu-left-wrapper">
         <button class="menu-toggle" (click)="toggleMenu()" aria-label="Toggle menu">
           &#9776;
         </button>
 
         <div class="top-menu-left" [class.open]="menuOpen">
-          <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{ exact: true }">Home</a>
-          <a routerLink="/news" routerLinkActive="active">News</a>
-          <a routerLink="/games" routerLinkActive="active">Games</a>
-          <a routerLink="/trivia" routerLinkActive="active">Trivia</a>
-          <a routerLink="/about" routerLinkActive="active">About</a>
+          <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{ exact: true }" (click)="closeMenu()">Home</a>
+          <a routerLink="/news" routerLinkActive="active" (click)="closeMenu()">News</a>
+          <a routerLink="/games" routerLinkActive="active" (click)="closeMenu()">Games</a>
+          <a routerLink="/trivia" routerLinkActive="active" (click)="closeMenu()">Trivia</a>
+          <a routerLink="/about" routerLinkActive="active" (click)="closeMenu()">About</a>
         </div>
       </div>
 
@@ -35,15 +35,18 @@ export class MenuComponent {
     this.menuOpen = !this.menuOpen;
   }
 
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
   onLogin() {
     alert('Login clicked!');
   }
 
-  // 👇 Close menu if click is outside the component
   @HostListener('document:click', ['$event'])
   handleClickOutside(event: Event) {
     if (!this.eRef.nativeElement.contains(event.target)) {
-      this.menuOpen = false;
+      this.closeMenu();
     }
   }
 }
