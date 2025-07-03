fd7ad9c76ba5ba281f5d7b82d4920638b5ce6f98
wenggxx <xiaoxi.weng@gmail.com>
2025-07-03 09:57:09 -0400

    menu bar in mobile device

--- ng-client/src/app/shared/menu/menu.component.ts
+++ ng-client/src/app/shared/menu/menu.component.ts
@@ -7,35 +7,29 @@ import { Router, RouterModule } from '@angular/router';
   imports: [RouterModule],
   template: `
     <nav class="top-menu">
-      <div class="top-menu-left">
+      <div class="top-menu-left" [class.open]="menuOpen">
         <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{ exact: true }">Home</a>
         <a routerLink="/news" routerLinkActive="active">News</a>
         <a routerLink="/games" routerLinkActive="active">Games</a>
         <a routerLink="/trivia" routerLinkActive="active">Trivia</a>
         <a routerLink="/about" routerLinkActive="active">About</a>
       </div>
+
       <button class="login-btn" (click)="onLogin()">Login</button>
+
+      <button class="menu-toggle" (click)="toggleMenu()" aria-label="Toggle menu">
+        &#9776;
+      </button>
     </nav>
   `,
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-  // constructor(private router: Router) {}
+  menuOpen = false;
 
-  // reloadTrivia(event: MouseEvent) {
-  //   event.preventDefault(); // prevent default link navigation
-
-  //   const currentUrl = this.router.url;
-
-  //   if (currentUrl === '/trivia') {
-  //     // Force a route reload
-  //     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-  //       this.router.navigate(['/trivia']);
-  //     });
-  //   } else {
-  //     this.router.navigate(['/trivia']);
-  //   }
-  // }
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 
   onLogin() {
     alert('Login clicked!');
